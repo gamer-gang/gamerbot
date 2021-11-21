ac7e5b08cff12afffafd4d7e52781397acf1141e
notrealn <56411504+notrealn@users.noreply.github.com>
2021-11-20 19:08:16 -0500

    fixed xkcd command, hope it works

--- src/commands/messages/xkcd.ts
+++ src/commands/messages/xkcd.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import command from '../command.js'
 
 const COMMAND_XKCD = command('CHAT_INPUT', {
@@ -7,8 +8,28 @@ const COMMAND_XKCD = command('CHAT_INPUT', {
   async run(context) {
     const { interaction } = context
 
-    await interaction.reply(`https://xkcd.com/${Math.floor(Math.random() * 2544) + 1}`)
+    await axios
+      .get('https://xkcd.com/info.0.json')
+      .then(async ({ data }: { data: XKCDResponse }) => {
+        const { num } = data
+        await interaction.reply(`https://xkcd.com/${Math.ceil(Math.random() * num)}`)
+      })
   },
 })
 
 export default COMMAND_XKCD
+
+export interface XKCDResponse {
+  month: string
+  num: number
+  link: string
+  year: string
+  news: string
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  safe_title: string
+  transcript: string
+  alt: string
+  img: string
+  title: string
+  day: string
+}
