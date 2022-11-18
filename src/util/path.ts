2351322a39ee415a65ca4a501b57db1672f640c0
wiisportsresort <devdoge1@gmail.com>
2022-11-18 07:31:44 -0500

    fix path again

--- src/util/path.ts
+++ src/util/path.ts
@@ -1,9 +1,14 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(
+let __filename = fileURLToPath(
   process.env.DOCKER != null ? 'file:///app/src/util/path.ts' : import.meta.url
 )
+
+if (__filename.endsWith('dist/index.js')) {
+  __filename = __filename.replace('dist/index.js', 'src/util/path.ts')
+}
+
 const __dirname = path.dirname(__filename)
 
 export const resolvePath = (dir: string): string => {
